4f9a485ede5f5691ac87422d5b79e8906d209683
Myles Pedronan <myles.pedronan@gmail.com>
2021-08-30 18:05:37 -0700

    Updated to include updateBook function
    Updated main file to include updateBook function and recompiled

--- main.cpp
+++ main.cpp
@@ -21,10 +21,10 @@ void addBook(){
   }
 
   try {
-    newBook = menu.addBookMenu(bookCount);
-  } catch(const char* msg) {
+    newBook = menu.addBookMenu();
+  } catch (const char* msg) {
     std::cerr << msg << "\n" << std::endl;
-    newBook = menu.addBookMenu(bookCount);
+    newBook = menu.addBookMenu();
   }
 
   newBook["bookID"] = bookCount;
@@ -37,7 +37,116 @@ void addBook(){
 
 }
 
-// void updateBook();
+void updateBook(int bookID){
+  json books, newBook;
+  int bookCount, updateOption, choice;
+  bool isFound;
+  std::string title, author;
+
+  try {
+    books = booksJSON.readBookJSON();
+    bookCount = books.size();
+  } catch (const char* msg){
+    std::cerr << msg << "\n" << std::endl;
+    // exit(EXIT_FAILURE);
+    return;
+  }
+
+  try{
+    updateOption = menu.updateBookMenu();
+  } catch (const char* msg){
+    std::cerr << msg << "\n" << std::endl;
+    return;
+  }
+
+  switch(updateOption){
+    case 1:
+      // Subject
+      std::cout << "What is the subject of the book [1: 'history', 2: 'science', 3: 'biography']: ";
+      std::cin >> choice;
+      if(std::cin.fail()){
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');  
+        throw "Invalid input. Please try again.";
+      } else if (choice < 0 || choice > 3){
+        throw "No valid choice selected. Please try again.";
+      }
+
+      if(choice == 1){
+        newBook["subject"] = "history";
+      } else if (choice == 2){
+        newBook["subject"] = "science";
+      } else {
+        newBook["subject"] = "biography";
+      }    
+
+      break;
+    case 2:
+      // Location
+      std::cout << "Which library is the book in [1: 'SF', 2: 'SM', 3: 'SJ']: ";
+      std::cin >> choice;
+      if(std::cin.fail()){
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');  
+        throw "Invalid input. Please try again.";
+      } else if (choice < 0 || choice > 3){
+        throw "No valid choice selected. Please try again.";
+      }
+
+      if(choice == 1){
+        newBook["location"] = "SF";
+      } else if (choice == 2){
+        newBook["location"] = "SM";
+      } else {
+        newBook["location"] = "SJ";
+      }
+
+      break;
+    case 3:
+      // Title
+      std::cout << "What is the title of the book: ";
+      std::cin >> title;
+      if(std::cin.fail()){
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');  
+        throw "Invalid input. Please try again.";
+      }
+      
+      newBook["bookInfo"]["title"] = title;
+
+      break;
+    case 4:
+      // Author
+      std::cout << "Who is the author of the book: ";
+      std::cin >> author;
+      if(std::cin.fail()){
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');  
+        throw "Invalid input. Please try again.";
+      }
+
+      newBook["bookInfo"]["author"] = author; 
+    case 5:
+      // Pub year
+      int pub;
+
+      std::cout << "What is the publication year: ";
+      std::cin >> pub;
+      if(std::cin.fail()){
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');  
+        throw "Invalid input. Please try again.";
+      } else if (pub < 1000 || pub > 2025){
+        throw "Invalid year. Please try again.";
+      }
+
+      newBook["bookInfo"]["pub"] = pub;     
+    default: 
+      break;
+  }
+
+  updateBook(bookID);
+}
 
 // void removeBook();
 
@@ -62,6 +171,15 @@ int main(){
       std::cout << "------------\t Update a Book\t------------" << "\n" << std::endl;
       std::cout << "Which book ID would you like to update: ";
       std::cin >> bookID;
+
+      if(std::cin.fail()){
+        std::cout << "Invalid option selected. Please try again." << "\n" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+      } 
+
+      updateBook(bookID);
+
       break;
     case 3:
       break;
@@ -77,8 +195,12 @@ int main(){
       break;
     case 8:
       break;
+    case 9:
+      exit(EXIT_SUCCESS);
+      break;
     default:
       std::cout << "Invalid choice. Please try again." << "\n" << std::endl;
+      break;
   }
 
   main();  

aab794bd1ad0dd6b903f37be6120ccd0df753f78
mylespedronan <myles.pedronan@gmail.com>
2021-09-09 11:07:17 -0700

    Updated main files and book.json

--- main.cpp
+++ main.cpp
@@ -2,151 +2,13 @@
 #include "inc/menu.hpp"
 #include "inc/json.hpp"
 #include "inc/json_handler.hpp"
+#include "inc/books.hpp"
 
 using json = nlohmann::json;
 
 Menu menu;
 BooksJSON booksJSON;
-
-void addBook(){
-  json books, newBook;
-  int bookCount;
-
-  try {
-    books = booksJSON.readBookJSON();
-    bookCount = books.size();             
-  } catch (const char* msg){
-    std::cerr << msg << "\n" << std::endl;
-    exit(EXIT_FAILURE);
-  }
-
-  try {
-    newBook = menu.addBookMenu();
-  } catch (const char* msg) {
-    std::cerr << msg << "\n" << std::endl;
-    newBook = menu.addBookMenu();
-  }
-
-  newBook["bookID"] = bookCount;
-  newBook["bookInfo"]["avail"] = true;
-
-  books += newBook;
-  booksJSON.updateBookJSON(books);
-
-  std::cout << "Book successfully added.\n" << std::endl;
-
-}
-
-void updateBook(int bookID){
-  json books, newBook;
-  int bookCount, updateOption, choice;
-  bool isFound;
-  std::string title, author;
-
-  try {
-    books = booksJSON.readBookJSON();
-    bookCount = books.size();
-  } catch (const char* msg){
-    std::cerr << msg << "\n" << std::endl;
-    // exit(EXIT_FAILURE);
-    return;
-  }
-
-  try{
-    updateOption = menu.updateBookMenu();
-  } catch (const char* msg){
-    std::cerr << msg << "\n" << std::endl;
-    return;
-  }
-
-  switch(updateOption){
-    case 1:
-      // Subject
-      std::cout << "What is the subject of the book [1: 'history', 2: 'science', 3: 'biography']: ";
-      std::cin >> choice;
-      if(std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(10000, '\n');  
-        throw "Invalid input. Please try again.";
-      } else if (choice < 0 || choice > 3){
-        throw "No valid choice selected. Please try again.";
-      }
-
-      if(choice == 1){
-        newBook["subject"] = "history";
-      } else if (choice == 2){
-        newBook["subject"] = "science";
-      } else {
-        newBook["subject"] = "biography";
-      }    
-
-      break;
-    case 2:
-      // Location
-      std::cout << "Which library is the book in [1: 'SF', 2: 'SM', 3: 'SJ']: ";
-      std::cin >> choice;
-      if(std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(10000, '\n');  
-        throw "Invalid input. Please try again.";
-      } else if (choice < 0 || choice > 3){
-        throw "No valid choice selected. Please try again.";
-      }
-
-      if(choice == 1){
-        newBook["location"] = "SF";
-      } else if (choice == 2){
-        newBook["location"] = "SM";
-      } else {
-        newBook["location"] = "SJ";
-      }
-
-      break;
-    case 3:
-      // Title
-      std::cout << "What is the title of the book: ";
-      std::cin >> title;
-      if(std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(10000, '\n');  
-        throw "Invalid input. Please try again.";
-      }
-      
-      newBook["bookInfo"]["title"] = title;
-
-      break;
-    case 4:
-      // Author
-      std::cout << "Who is the author of the book: ";
-      std::cin >> author;
-      if(std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(10000, '\n');  
-        throw "Invalid input. Please try again.";
-      }
-
-      newBook["bookInfo"]["author"] = author; 
-    case 5:
-      // Pub year
-      int pub;
-
-      std::cout << "What is the publication year: ";
-      std::cin >> pub;
-      if(std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(10000, '\n');  
-        throw "Invalid input. Please try again.";
-      } else if (pub < 1000 || pub > 2025){
-        throw "Invalid year. Please try again.";
-      }
-
-      newBook["bookInfo"]["pub"] = pub;     
-    default: 
-      break;
-  }
-
-  updateBook(bookID);
-}
+Books books;
 
 // void removeBook();
 
@@ -165,10 +27,9 @@ int main(){
   switch(choice){
     case 1:
       std::cout << "------------\t Add a Book\t------------" << "\n" << std::endl;
-      addBook();
+      books.addBook();
       break;
     case 2:
-      std::cout << "------------\t Update a Book\t------------" << "\n" << std::endl;
       std::cout << "Which book ID would you like to update: ";
       std::cin >> bookID;
 
@@ -178,24 +39,72 @@ int main(){
         std::cin.ignore(10000, '\n');
       } 
 
-      updateBook(bookID);
+      books.updateBook(bookID);
 
       break;
     case 3:
+      std::cout << "Which book ID would you like to delete: ";
+      std::cin >> bookID;
+
+      if(std::cin.fail()){
+        std::cout << "Invalid option selected. Please try again." << "\n" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+      } 
+
+      books.deleteBook(bookID);
       break;
     case 4:
       std::cout << "------------\t All Book Info\t------------" << "\n" << std::endl;
+      books.listAllBooks();
       break;
     case 5:
+      std::cout << "Which book ID would you like to list: ";
+      std::cin >> bookID;
+
+      if(std::cin.fail()){
+        std::cout << "Invalid option selected. Please try again." << "\n" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+      } 
+
+      std::cout << std::endl;
       std::cout << "------------\tBook Info\t------------" << "\n" << std::endl;
+
+      books.listBook(bookID);
       break;
     case 6:
+      std::cout << "Which book ID would you like to rent: ";
+      std::cin >> bookID;
+
+      if(std::cin.fail()){
+        std::cout << "Invalid option selected. Please try again." << "\n" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+      } 
+
+      std::cout << std::endl;    
+
+      books.rentBook(bookID);
       break;
     case 7:
+      std::cout << "Which book ID would you like to return: ";
+      std::cin >> bookID;
+
+      if(std::cin.fail()){
+        std::cout << "Invalid option selected. Please try again." << "\n" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(10000, '\n');
+      } 
+
+      std::cout << std::endl;    
+
+      books.returnBook(bookID);
       break;
     case 8:
       break;
     case 9:
+      std::cout << "Exiting Program.\n" << std::endl;
       exit(EXIT_SUCCESS);
       break;
     default:
